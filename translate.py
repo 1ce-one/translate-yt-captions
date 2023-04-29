639f02c24fd0b904397c4246fc04a55b277c90a7
Nbdyleto <nobodyleto2004@gmail.com>
2023-04-29 07:11:13 -0300

    Remove unnecessary functions

--- translate.py
+++ translate.py
@@ -8,52 +8,6 @@ import html
 import urllib.parse
 
 class EasyGoogleTranslate:
-
-    '''
-        Unofficial Google Translate API. 
-
-        This library does not need an api key or something else to use, it's free and simple.
-        You can either use a string or a file to translate but the text must be equal to or less than 5000 character. 
-        You can split your text into 5000 characters to translate more.
-
-        Google Translate supports 108 different languages. You can use any of them as source and target language in this application.
-        If source language is not specified, it will detect source language automatically.
-        This application supports multi thread translation, you can use it to translate multiple languages at once.
-        Detailed language list can be found here:  https://cloud.google.com/translate/docs/languages
-
-
-        Examples:
-            #1: Specify default source and target language at beginning and use it any time.
-                translator = GoogleTranslateRequest(
-                    source_language='en',
-                    target_language='de',
-                    timeout=10
-                )
-                result = translator.translate('This is an example.')
-                print(result)
-
-            #2: Don't specify default parameters.
-                translator = GoogleTranslateRequest()
-                result = translator.translate('This is an example.', target_language='tr')
-                print(result)
-
-            #2: Override default parameters.
-                translator = GoogleTranslateRequest(target_language='tr')
-                result = translator.translate('This is an example.', target_language='fr')
-                print(result)
-
-            #4: Translate a text in multiple languages at once via multi-threading.
-                translator = GoogleTranslateRequest()
-                result = translator.translate(text='This is an example.', target_language=['tr', 'fr', 'de'])
-                print(result)
-
-            #5: Translate a file in multiple languages at once via multi-threading.
-                translator = GoogleTranslateRequest()
-                result = translator.translate_file(file_path='text.txt', target_language=['tr', 'fr', 'de'])
-                print(result)
-
-    '''
-
     def __init__(self, source_language='auto', target_language='tr', timeout=5):
         self.source_language = source_language
         self.target_language = target_language
@@ -90,15 +44,6 @@ class EasyGoogleTranslate:
                 return_value = [f.result() for f in futures]
                 return return_value
         return self.make_request(target_language, source_language, text, timeout)
-
-    def translate_file(self, file_path, target_language='', source_language='', timeout=''):
-        if not os.path.isfile(file_path):
-            print('\nError: The file or path is incorrect.')
-            exit(0)
-        f = open(file_path)
-        text = self.translate(f.read(), target_language, source_language, timeout)
-        f.close()
-        return text
 
 def get_text_from_srt():
     file = open( "files-to-test/js-netflix-doco.srt", "r")
